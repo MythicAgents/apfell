a57e113944566a90dae61d68f3d43c24bfa89d6c
Antonio Piazza <antman1p@gmail.com>
2021-09-08 16:24:23 -0500

    Debugging call to chainbreaker

--- Payload_Type/apfell/mythic/agent_functions/cookie_thief.py
+++ Payload_Type/apfell/mythic/agent_functions/cookie_thief.py
@@ -2,7 +2,7 @@ from mythic_payloadtype_container.MythicCommandBase import *
 import json
 import os
 from mythic_payloadtype_container.MythicRPC import *
-import chainbreaker
+from chainbreaker import *
 
 
 class CookieThiefArguments(TaskArguments):
