42eef449faa9494c4a36befe3acb79c2973ce3ed
its-a-feature <codybthomas@gmail.com>
2023-04-14 11:15:37 -0500

    updating `upload` command to use post_response with upload key

--- Payload_Type/apfell/apfell/agent_code/upload.js
+++ Payload_Type/apfell/apfell/agent_code/upload.js
@@ -14,17 +14,17 @@ exports.upload = function(task, command, params){
         else{
             data = write_data_to_file(data, full_path);
             try{
-	            let fm = $.NSFileManager.defaultManager;
-	            let pieces = ObjC.deepUnwrap(fm.componentsToDisplayForPath(full_path));
-	            if(pieces === undefined){
-	                return {'status': 'error', 'user_output': String(data), 'completed': true};
+                let fm = $.NSFileManager.defaultManager;
+                let pieces = ObjC.deepUnwrap(fm.componentsToDisplayForPath(full_path));
+                if(pieces === undefined){
+                    return {'status': 'error', 'user_output': String(data), 'completed': true};
                 }
-	            full_path = "/" + pieces.slice(1).join("/");
-	        }catch(error){
-	            return {'status': 'error', 'user_output': error.toString(), 'completed': true};
-	        }
-            return {"user_output":String(data), "completed": true, 'full_path': full_path, "file_id": file_id,
-            "artifacts": [{"base_artifact": "File Create", "artifact": full_path}]};
+                full_path = "/" + pieces.slice(1).join("/");
+            }catch(error){
+                return {'status': 'error', 'user_output': error.toString(), 'completed': true};
+            }
+            return {"user_output":String(data), "completed": true, "upload": {'full_path': full_path, "file_id": file_id},
+                "artifacts": [{"base_artifact": "File Create", "artifact": full_path}]};
         }
     }catch(error){
         return {"user_output":error.toString(), "completed": true, "status": "error"};
